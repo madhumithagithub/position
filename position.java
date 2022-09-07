1280ee913edc1081c007f96d8aa321426aa9106f
madhumithagithub <112962387+madhumithagithub@users.noreply.github.com>
2022-09-07 14:23:40 +0530

    Add files via upload

---
+++ position.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	   long a=1,n=50,i;
+	   Scanner sc=new Scanner(System.in);
+	   long b=sc.nextLong();
+	   for(i=1;i<=b;i++)
+	   {
+	      a= a*2;
+	     
+	   }
+	   System.out.println(a);
+	}
+}
